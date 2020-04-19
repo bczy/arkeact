0c58506c0e5be412f5598b8039749516de74a664
trambz <bertrand.coizy@gmail.com>
2020-04-19 08:16:47 +0200

    Reseting direction after ball loss

--- src/components/game/Ball.js
+++ src/components/game/Ball.js
@@ -11,10 +11,14 @@ export function Ball() {
     args: 1,
     position: [0, 0, 0],
   }));
-
   useFrame((state) => {
     // The paddle is kinematic (not subject to gravitation), we move it with the api returned by useBox
-    if (!ballLaunched) api.position.set(state.mouse.x * 10, state.mouse.y * 10, -1.5);
+    if (!ballLaunched) {
+      api.position.set(state.mouse.x * 10, state.mouse.y * 10, -1.5);
+      // Velocity
+      api.velocity.set(0, 0, 0, 1);
+      api.angularVelocity.set(0, 0, 0, 1);
+    }
   });
   return (
     <mesh castShadow receiveShadow ref={ref}>
