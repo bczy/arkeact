cfd70c637ac53bd4c30cb34089aeefd8b3265f19
trambz <bertrand.coizy@gmail.com>
2020-04-26 14:41:16 +0200

    Tweaking Light

--- src/components/game/Lights.js
+++ src/components/game/Lights.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Vector3 } from 'three';
 
 export function Lights() {
   return (
     <>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.75} />
       <directionalLight
         position={[0, 0, 10]}
         shadow-camera-near={0.1}
