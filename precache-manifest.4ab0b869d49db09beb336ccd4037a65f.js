32afe48f28f2ddbc93d3ac06e854b94f5c76af2a
trambz <bertrand.coizy@gmail.com>
2020-04-29 13:10:51 +0200

    Updates

---
+++ precache-manifest.4ab0b869d49db09beb336ccd4037a65f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d501d8b5915ebf490436ef79ef18fb07",
+    "url": "/arkareact/index.html"
+  },
+  {
+    "revision": "01a6f1bfd24c279dbad6",
+    "url": "/arkareact/static/css/main.52f15fef.chunk.css"
+  },
+  {
+    "revision": "ee4fb0588fca5721cdd5",
+    "url": "/arkareact/static/js/2.708170b4.chunk.js"
+  },
+  {
+    "revision": "b803c26b5bb963d4a54b0b1b419ee973",
+    "url": "/arkareact/static/js/2.708170b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9137a1ab8e515f4d9bf",
+    "url": "/arkareact/static/js/3.28c2fb60.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/arkareact/static/js/3.28c2fb60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a6f1bfd24c279dbad6",
+    "url": "/arkareact/static/js/main.9551db82.chunk.js"
+  },
+  {
+    "revision": "b8360f5dd4c5a83734ac",
+    "url": "/arkareact/static/js/runtime-main.4a975ea5.js"
+  },
+  {
+    "revision": "eed03f1340817f8c6513f2a7fbfcf9cd",
+    "url": "/arkareact/static/media/brick.eed03f13.mp3"
+  },
+  {
+    "revision": "00397b42275e8fba8bec2f8d8ab982f7",
+    "url": "/arkareact/static/media/cross.00397b42.jpg"
+  },
+  {
+    "revision": "b6bfcd7985081aee7420fcef04b54675",
+    "url": "/arkareact/static/media/gameOver.b6bfcd79.mp3"
+  },
+  {
+    "revision": "9cae2b3e8fb92cfd706b11a445ab6ca6",
+    "url": "/arkareact/static/media/tryAgain.9cae2b3e.mp3"
+  },
+  {
+    "revision": "4c0964a4244111593c98791bcd1fd7c3",
+    "url": "/arkareact/static/media/wall.4c0964a4.mp3"
+  }
+]);
