b93f7ae3e40c5fe4b038f92c986acc87c750db02
trambz <bertrand.coizy@gmail.com>
2020-04-29 13:27:54 +0200

    Adding particle to tile explosion

--- src/components/game/Box.js
+++ src/components/game/Box.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
+import * as THREE from 'three';
+import { useThree, useFrame } from 'react-three-fiber';
 import { useBox } from 'use-cannon';
 
-import { useGameStore } from '../../data/stores/game';
-
 import UIFx from 'uifx';
+
+import { useGameStore } from '../../data/stores/game';
 
 import brick from '../../assets/sounds/brick.mp3';
 import wall from '../../assets/sounds/wall.mp3';
@@ -12,11 +14,43 @@ import wall from '../../assets/sounds/wall.mp3';
 import { GameContext } from '../../data/game-context';
 
 export function Box({ position, size = [2, 2, 2], userData, id }) {
-  const { retrieveBall } = useGameStore();
+  const { retrieveBall, resetBall } = useGameStore();
   const { setGlitching } = useContext(GameContext);
 
   const isWall = isNaN(userData.strength);
   const isCorner = isNaN(userData.isCorner);
+  const [particleSystem, setParticleSystem] = useState();
+
+  function buildParticleSystem() {
+    // create the particle variables
+    const particleCount = 25;
+    const particles = new THREE.Geometry();
+    const pMaterial = new THREE.PointsMaterial({
+      color: userData.color,
+      size: 0.9,
+      transparent: true,
+      depthTest: false,
+      sizeAttenuation: true,
+      opacity: 0.9,
+    });
+
+    // now create the individual particles
+    for (let p = 0; p < particleCount; p++) {
+      // create a particle with random
+      // position values, -250 -> 250
+      const pX = Math.random() * 2.5 - 1.25 + ref.current.position.x;
+      const pY = Math.random() * 2.5 - 1.25 + ref.current.position.y;
+      const pZ = Math.random() * 2.5 - 1.25 + ref.current.position.z;
+      const particle = new THREE.Vector3(pX, pY, pZ);
+      // add it to the geometry
+      particles.vertices.push(particle);
+    }
+
+    const ps = new THREE.Points(particles, pMaterial);
+    ps.customRotation = Math.random() * 2 - 1;
+    scene.add(ps);
+    return ps;
+  }
 
   let soundAsset = null;
   if (isWall) {
@@ -24,7 +58,10 @@ export function Box({ position, size = [2, 2, 2], userData, id }) {
   } else if (!userData.isRoof) {
     soundAsset = brick;
   }
+
   const hitSound = new UIFx(soundAsset);
+
+  const { scene } = useThree();
 
   const [ref, api] = useBox(() => {
     return {
@@ -37,19 +74,31 @@ export function Box({ position, size = [2, 2, 2], userData, id }) {
         if (!isWall) {
           userData.strength--;
           api.position.set(-1000, -1000, -100);
+          setParticleSystem(buildParticleSystem());
         } else if (userData.isRoof) {
-          retrieveBall();
+          resetBall();
           setGlitching(true);
-          setTimeout(() => setGlitching(false), 600);
+          setTimeout(() => {
+            setGlitching(false);
+            retrieveBall();
+          }, 300);
         }
       },
     };
   });
 
+  useFrame(() => {
+    if (particleSystem && particleSystem.material.opacity > 0) {
+      particleSystem.material.opacity -= 0.0075;
+      particleSystem.scale.x += 0.01;
+      particleSystem.scale.y += 0.01;
+      particleSystem.rotation.x += particleSystem.customRotation / 50;
+    }
+  });
+
   return (
     <mesh key={id} ref={ref} receiveShadow userData={userData}>
       <boxGeometry attach="geometry" args={size} />
-
       <meshStandardMaterial attach="material" color={userData.color} />
     </mesh>
   );
