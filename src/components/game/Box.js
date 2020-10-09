d482ced5b63685a7dfe6e58adacd0bbe01f9ade5
trambz <bertrand.coizy@gmail.com>
2020-10-09 09:29:52 +0200

    Refactoring / Linting

--- src/components/game/Box.js
+++ src/components/game/Box.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useLayoutEffect, useState } from 'react';
 
 import * as THREE from 'three';
@@ -11,119 +13,132 @@ import wall from '../../assets/sounds/wall.mp3';
 
 import { gameStore } from '../../stores/gameStore';
 
-function createParticals(particleCount, ref){
-  const particles = new THREE.Geometry();
-  
-  // TODO: object pooling
-  for (let p = 0; p < particleCount; p++) {
-    // create a particle with random
-    // position values, -250 -> 250
-    const pX = Math.random() * 2.5 - 1.25 + ref.current.position.x;
-    const pY = Math.random() * 2.5 - 1.25 + ref.current.position.y;
-    const pZ = Math.random() * 2.5 - 1.25 + ref.current.position.z;
-    const particle = new THREE.Vector3(pX, pY, pZ);
-    // add it to the geometry
-    particles.vertices.push(particle);
-  }
-  return particles;
+function createParticals(particleCount, ref) {
+	const particles = new THREE.Geometry();
+	// TODO: object pooling
+	for (let p = 0; p < particleCount; p++) {
+		// create a particle with random
+		// position values, -250 -> 250
+		const pX = Math.random() * 2.5 - 1.25 + ref.current.position.x;
+		const pY = Math.random() * 2.5 - 1.25 + ref.current.position.y;
+		const pZ = Math.random() * 2.5 - 1.25 + ref.current.position.z;
+		const particle = new THREE.Vector3(pX, pY, pZ);
+		// add it to the geometry
+		particles.vertices.push(particle);
+	}
+	return particles;
 }
-function createMaterial(color){
-  return new THREE.PointsMaterial({
-    color,
-    size: 0.9,
-    transparent: true,
-    depthTest: false,
-    sizeAttenuation: true,
-    opacity: 0.9,
-  });
-}
+const createMaterial = (color) =>
+	new THREE.PointsMaterial({
+		color,
+		size: 0.9,
+		transparent: true,
+		depthTest: false,
+		sizeAttenuation: true,
+		opacity: 0.9,
+	});
+
 export function Box({ position, size = [2, 2, 2], userData, id }) {
-  const { strength, color, fillColor, cornerData, isRoof, isTile, scoreValue } = userData
+	const {
+		strength,
+		color,
+		fillColor,
+		cornerData,
+		isRoof,
+		isTile,
+		scoreValue,
+	} = userData;
 
-  const [balls, setBalls] = useState(3);
+	const [balls, setBalls] = useState(3);
 
-  useLayoutEffect(() => {
-    const subs = gameStore.balls.subscribe(setBalls);
-    return () => subs.unsubscribe();
-  }, []);
+	useLayoutEffect(() => {
+		const subs = gameStore.balls.subscribe(setBalls);
+		return () => subs.unsubscribe();
+	}, []);
 
-  const isWall = isNaN(strength);
-  const isCorner = isNaN(cornerData);
-  const [particleSystem, setParticleSystem] = useState();
+	const isWall = isNaN(strength);
+	const isCorner = isNaN(cornerData);
+	const [particleSystem, setParticleSystem] = useState();
 
-  function buildParticleSystem() {
-    const material = createMaterial(color)
-    const particles = createParticals(25, ref, material)
+	function buildParticleSystem() {
+		const material = createMaterial(color);
+		const particles = createParticals(25, ref, material);
 
-    const ps = new THREE.Points(particles, material);
-    ps.customRotation = Math.random() * 2 - 1;
-    scene.add(ps);
-    return ps;
-  }
-  
-  let soundAsset = null;
-  if (isWall || isCorner) {
-    soundAsset = wall;
-  } else if (isRoof || isTile) {
-    soundAsset = brick;
-  }
-  const hitSound = new UIFx(soundAsset);
+		const ps = new THREE.Points(particles, material);
+		ps.customRotation = Math.random() * 2 - 1;
+		scene.add(ps);
+		return ps;
+	}
 
-  const { scene } = useThree();
+	let soundAsset = null;
+	if (isWall || isCorner) {
+		soundAsset = wall;
+	} else if (isRoof || isTile) {
+		soundAsset = brick;
+	}
+	const hitSound = new UIFx(soundAsset);
 
-  const [ref, api] = useBox(() => {
-    return {
-      type: isCorner ? 'Static' : 'Kinematic',
-      args: size.map((x) => x / 2),
-      position,
-      userData,
-      onCollide: (e) => {
-        hitSound.play();
-        if (isRoof) {
-          gameStore.resetBall();
-          gameStore.setGlitching(true);
-          setTimeout(() => {
-            gameStore.setGlitching(false);
-            setBalls(balls - 1);
-          }, 300);
-        } else if (!isWall) {
-          userData.strength--;
-          if (userData.strength <= 0) {
-            api.position.set(-1000, -1000, -100);
-            setParticleSystem(buildParticleSystem(45));
-            gameStore.increasescoreValue(scoreValue);
-          }
-          else{
-            setParticleSystem(buildParticleSystem(15));
-          }
-        }
-      },
-    };
-  });
+	const { scene } = useThree();
 
-  useFrame(() => {
-    if (particleSystem && particleSystem.material.opacity > 0) {
-      particleSystem.material.opacity -= 0.0075;
-      particleSystem.scale.x += 0.01;
-      particleSystem.scale.y += 0.01;
-      particleSystem.rotation.y += particleSystem.customRotation / 50;
-    }
-  });
+	const [ref, api] = useBox(() => {
+		return {
+			type: isCorner ? 'Static' : 'Kinematic',
+			args: size.map((x) => x / 2),
+			position,
+			userData,
+			onCollide: (e) => {
+				hitSound.play();
+				if (isRoof) {
+					gameStore.resetBall();
+					gameStore.setGlitching(true);
+					setTimeout(() => {
+						gameStore.setGlitching(false);
+						setBalls(balls - 1);
+					}, 300);
+				} else if (!isWall) {
+					userData.strength--;
+					if (userData.strength <= 0) {
+						api.position.set(-1000, -1000, -100);
+						setParticleSystem(buildParticleSystem(45));
+						gameStore.increaseScoreValue(scoreValue);
+					} else {
+						setParticleSystem(buildParticleSystem(15));
+					}
+				}
+			},
+		};
+	});
 
-  return (<>{
-    isTile ? 
-      (<mesh key={id} ref={ref} userData={userData}>
-        <boxGeometry attach="geometry" args={size} />
-        <meshStandardMaterial attach="material" wireframe={true} color={color} />
-        <mesh receiveShadow userData={userData}>
-          <boxGeometry attach="geometry" args={size.map(i => i *0.99)} />
-          <meshStandardMaterial attach="material" color={fillColor}  />
-        </mesh>
-      </mesh>) : 
-      (<mesh key={id} ref={ref} receiveShadow  userData={userData}>
-        <boxGeometry attach="geometry" args={size} receiveShadow  />
-        <meshStandardMaterial attach="material" receiveShadow color={color} />
-      </mesh>)
-    }</>)
-  ;
+	useFrame(() => {
+		if (particleSystem && particleSystem.material.opacity > 0) {
+			particleSystem.material.opacity -= 0.0075;
+			particleSystem.scale.x += 0.01;
+			particleSystem.scale.y += 0.01;
+			particleSystem.rotation.y += particleSystem.customRotation / 50;
+		}
+	});
+
+	return (
+		<>
+			{isTile ? (
+				<mesh key={id} ref={ref} userData={userData}>
+					<boxGeometry attach="geometry" args={size} />
+					<meshStandardMaterial
+						attach="material"
+						wireframe={true}
+						color={color}
+					/>
+					<mesh receiveShadow userData={userData}>
+						<boxGeometry attach="geometry" args={size.map((i) => i * 0.99)} />
+						<meshStandardMaterial attach="material" color={fillColor} />
+					</mesh>
+				</mesh>
+			) : (
+				<mesh key={id} ref={ref} receiveShadow userData={userData}>
+					<boxGeometry attach="geometry" args={size} receiveShadow />
+					<meshStandardMaterial attach="material" receiveShadow color={color} />
+				</mesh>
+			)}
+		</>
+	);
 }
--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -1,66 +1,73 @@
+/** @format */
+
 import { Subject, BehaviorSubject } from 'rxjs';
 import { levelStore } from './levelStore';
 import { playerStore } from './playerStore';
 
-export const GAME_STATES = { WELCOME: 0, LEVEL_CHOICE: 1, GAME: 2, LEVEL_DEBRIEF: 3 };
+export const GAME_STATES = {
+	WELCOME: 0,
+	LEVEL_CHOICE: 1,
+	GAME: 2,
+	LEVEL_DEBRIEF: 3,
+};
 class GameStore {
-  constructor() {
-    if (!GameStore.instance) {
-      GameStore.instance = this;
-      this.gameState = new BehaviorSubject(GAME_STATES.WELCOME);
-      this.nbBrickDestroyed = new BehaviorSubject(0);
-      this.balls = new BehaviorSubject(3);
-      this.scoreValue = new BehaviorSubject(0);
-      this.inGame = new Subject(false);
-      this.ballLaunched = new Subject(false);
-      this.currentLevel = new BehaviorSubject(1);
-      this.glitching = new Subject(false);
-    }
-    return GameStore.instance;
-  }
-  setGameState = (gameState) => {
-    if (gameState === GAME_STATES.LEVEL_CHOICE) {
-    }
-    this.gameState.next(gameState);
-  };
-  launchLevel = (levelId) => {
-    this.currentLevel.next(levelId);
-    this.gameState.next(GAME_STATES.GAME);
-  };
-  increaseLevelNumber = () => {
-    this.currentLevel.next(this.currentLevel.value + 1);
-  }
-  launchGame = () => {
-    this.resetGame();
-    this.inGame.next(true);
-  };
-  launchBall = () => {
-    this.ballLaunched.next(true);
-  };
-  resetBall = () => {
-    if (this.balls.value > 0) {
-      this.balls.next(this.balls.value - 1);
-      this.ballLaunched.next(false);
-    } else {
-      this.gameState.next(GAME_STATES.LEVEL_DEBRIEF);
-    }
-  };
-  resetGame = () => {
-    this.balls.next(3);
-    this.nbBrickDestroyed.next(0);
-    this.scoreValue.next(0);
-    this.inGame.next(false);
-    this.ballLaunched.next(false);
-  };
-  setGlitching = (value) => this.glitching.next(value);
-  increasescoreValue = (scoreValue) => {
-    this.scoreValue.next(this.scoreValue.value + scoreValue);
-    if (this.nbBrickDestroyed.value + 1 === levelStore.tiles.value.length) {
-      playerStore.addHighscoreValue();
-      this.gameState.next(GAME_STATES.LEVEL_DEBRIEF);
-    }
-    this.nbBrickDestroyed.next(this.nbBrickDestroyed.value + 1);
-  };
+	constructor() {
+		if (!GameStore.instance) {
+			GameStore.instance = this;
+			this.gameState = new BehaviorSubject(GAME_STATES.WELCOME);
+			this.nbBrickDestroyed = new BehaviorSubject(0);
+			this.balls = new BehaviorSubject(3);
+			this.scoreValue = new BehaviorSubject(0);
+			this.inGame = new Subject(false);
+			this.ballLaunched = new Subject(false);
+			this.currentLevel = new BehaviorSubject(1);
+			this.glitching = new Subject(false);
+		}
+		return GameStore.instance;
+	}
+	setGameState = (gameState) => {
+		if (gameState === GAME_STATES.LEVEL_CHOICE) {
+		}
+		this.gameState.next(gameState);
+	};
+	launchLevel = (levelId) => {
+		this.currentLevel.next(levelId);
+		this.gameState.next(GAME_STATES.GAME);
+	};
+	increaseLevelNumber = () => {
+		this.currentLevel.next(this.currentLevel.value + 1);
+	};
+	launchGame = () => {
+		this.resetGame();
+		this.inGame.next(true);
+	};
+	launchBall = () => {
+		this.ballLaunched.next(true);
+	};
+	resetBall = () => {
+		if (this.balls.value > 0) {
+			this.balls.next(this.balls.value - 1);
+			this.ballLaunched.next(false);
+		} else {
+			this.gameState.next(GAME_STATES.LEVEL_DEBRIEF);
+		}
+	};
+	resetGame = () => {
+		this.balls.next(3);
+		this.nbBrickDestroyed.next(0);
+		this.scoreValue.next(0);
+		this.inGame.next(false);
+		this.ballLaunched.next(false);
+	};
+	setGlitching = (value) => this.glitching.next(value);
+	increaseScoreValue = (scoreValue) => {
+		this.scoreValue.next(this.scoreValue.value + scoreValue);
+		if (this.nbBrickDestroyed.value + 1 === levelStore.tiles.value.length) {
+			playerStore.addHighscoreValue();
+			this.gameState.next(GAME_STATES.LEVEL_DEBRIEF);
+		}
+		this.nbBrickDestroyed.next(this.nbBrickDestroyed.value + 1);
+	};
 }
 
 export const gameStore = new GameStore();
