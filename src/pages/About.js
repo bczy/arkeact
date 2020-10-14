cfd5e0723a8110aee10865325de0d58e377deb19
trambz <bertrand.coizy@gmail.com>
2020-10-14 10:35:11 +0200

    Adding temporary about content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,23 @@
 /** @format */
 
 import React from 'react';
+import styled from 'styled-components';
+
 import { PageContainer } from '../components/hud/common/PageContainer';
+
+const AboutContent = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column; 
+    font-size: 1.5em;
+`
 
 export const About = () => 
     <PageContainer title={"ABOUT"}>
-        <p>Hello there</p>
+        <AboutContent>
+            <p>This game is a side project started during my confinment.</p>
+            <p>Made with react-three-fiber, canonjs, .</p>
+            <p>A special thanks to <a href="/#">Mat'</a></p>
+        </AboutContent>
     </PageContainer>
 
