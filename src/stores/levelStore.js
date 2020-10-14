a45f3d79dc92e319fc8856bea684dc9985d965bc
trambz <bertrand.coizy@gmail.com>
2020-10-14 09:25:26 +0200

    Fixing last level selection.

--- src/stores/levelStore.js
+++ src/stores/levelStore.js
@@ -15,7 +15,7 @@ class LevelStore {
 
   getLockedLevels(bestscoreValues){
     const levels = LEVELS.levels;
-    return [...levels].splice(bestscoreValues.length - 1, levels.length - 1);
+    return [...levels].splice(bestscoreValues.length, levels.length - 1);
   }
 }
 
